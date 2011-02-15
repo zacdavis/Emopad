4930c588e43be83f1bb84f8abead133b0921de9a
Zac Davis <zactronica@gmail.com>
2011-02-14 20:38:17 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emopad::Application.config.secret_token = '1bf82175979c9572579f6381e4c7b45371391569f77a0164c0021834c36f94407cccce8e3d0db074cba4598d3c1235de337afbc083dfee3b33cd8f5fa94421fd'
