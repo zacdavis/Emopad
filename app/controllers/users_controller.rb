a1e3f4c119f4edbb01cfec10a38586bb13babfe7
Zac Davis <zactronica@gmail.com>
2011-03-02 02:01:05 -0600

    Done with sign in (again)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create 
   @user = User.new(params[:user])
   	if @user.save 
-  	flash[:success] = "Welcome to emopad"
+  	flash[:success] = "Welcome to Emopad"
   	 redirect_to @user
   	else
   	@title = "Sign up"
